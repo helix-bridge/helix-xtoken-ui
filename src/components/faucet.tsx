59134e944e9088cedee1d22d67da00711e1beda3
JayJay1024 <lygm1062@gmail.com>
2024-03-27 19:06:41 +0800

    refactor: update faucet item pending-y

--- src/components/faucet.tsx
+++ src/components/faucet.tsx
@@ -149,7 +149,7 @@ export default function Faucet() {
 
 function Item({ value, token }: { value: bigint; token?: Token }) {
   return (
-    <div className="flex items-center justify-between rounded-xl bg-inner px-middle py-middle">
+    <div className="flex items-center justify-between rounded-xl bg-inner px-middle py-3">
       <span className="text-sm font-semibold">{token && formatBalance(value, token.decimals)}</span>
       <span className="text-sm font-semibold">{token?.symbol}</span>
     </div>

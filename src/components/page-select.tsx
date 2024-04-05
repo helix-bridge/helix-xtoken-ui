57d3c872e1d8a0e359dc756587ac322d0b5b2d97
JayJay1024 <lygm1062@gmail.com>
2024-04-05 11:33:40 +0800

    fix: fix home page select

--- src/components/page-select.tsx
+++ src/components/page-select.tsx
@@ -3,7 +3,7 @@
 import HomePage from "./home-page";
 import ProjectPage from "./project-page";
 
-const isHomePage = window.location.hostname.split(".").includes("darwinia");
+const isHomePage = !window.location.hostname.split(".").includes("darwinia");
 
 export default function PageSelect() {
   return isHomePage ? <HomePage /> : <ProjectPage />;
